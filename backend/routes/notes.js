89db2c7046ed67457cc6f2b1e20bea99d692c114
Arpit Hamjade <arpitshirishhamjade@gmail.com>
2024-03-30 22:44:14 +0530

    added deletenote endpoint

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,11 +45,14 @@ router.post(
   }
 );
 
-//Update an existing notes using :GET  "/api/notes/updatenote" .
+//Update an existing notes using :PUT  "/api/notes/updatenote" .
 router.put(
   "/updatenote/:id",fetchuser,async (req, res) => {
     const { title, description, tag } = req.body;
     // Create a newNote obj
+    try {
+      
+
     const newNote={};
     if (title) { newNote.title=title;}
     if (description) {newNote.description=description;}
@@ -66,6 +69,33 @@ router.put(
     // Update the note with the new data
     note = await Note.findByIdAndUpdate(req.params.id ,{$set:newNote}, {new:true});
     res.json(note);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+  });
+
+
+//Delete an existing notes using :DELETE  "/api/notes/deletenote" .
+router.delete(
+  "/deletenote/:id",fetchuser,async (req, res) => {
+    try {
+    //Find the note to be deleted
+    let  note = await Note.findById(req.params.id);
+    if(!note){
+      return res.send(404).send("Not Found");
+    }
+    //Allow deletion only for owner of the note
+    if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+    // Update the note with the new data
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({"Success":"Note has been deleted",note:note});
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
   });
 
 module.exports = router;
